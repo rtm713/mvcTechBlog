ff58f26de75028c70e87531cc8e685e6345a3cb5
rtm713 <rtm713@gmail.com>
2023-08-08 03:16:12 -0500

    completed login and signup pagse routes and styling

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 const loginRoutes = require('./loginRoutes');
+const signupRoutes = require('./signupRoutes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/login', loginRoutes);
+router.use('/signup', signupRoutes);
 
 module.exports = router;
